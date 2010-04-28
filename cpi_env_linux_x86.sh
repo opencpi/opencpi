2ef9e1e4def3de1b57c836b13808f6a46737adaf
Michael Pepe <mpepe@mercfed.com>
2010-04-28 12:29:58 -0400

    Modifications made to build OpenCPI for x86_64 RHEL 5.

---
+++ cpi_env_linux_x86.sh
@@ -0,0 +1,16 @@
+# Build from Linux for Linux
+export ORB=OMNI
+export PPP_LIB=
+export PPP_INC=
+export CPIDIR=/home/mpepe/projects/jcrew/i2wd/opencpi
+export SYSTEM=linux
+export ARCH=x86
+export BUILDSHAREDLIBRARIES=1
+export DEBUG=1
+export ASSERT=1
+export USE_CPIP_SIMULATION=0
+export HAVE_CORBA=1
+export ACE_ROOT=/opt/local/TAO/5.6.6/linux-x86_64-gcc/ACE_wrappers
+export HOST_ROOT=/opt/local/TAO/5.6.6/linux-x86_64-gcc/ACE_wrappers
+export LD_LIBRARY_PATH=/opt/local/TAO/5.6.6/linux-x86_64-gcc/ACE_wrappers/lib:$LD_LIBRARY_PATH
+export OUTDIR=$SYSTEM-$ARCH-bin
