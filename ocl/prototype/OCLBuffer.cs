21bcc907ea74456516ded1824a6ded606b43e0d9
U-VALUED-474CCF39\Chuck <Chuck@VALUED-474CCF39.(none)>
2011-03-09 16:54:50 -0600

    Multiple changes

--- ocl/prototype/OCLBuffer.cs
+++ ocl/prototype/OCLBuffer.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.InteropServices;
+using Cloo;
 
 namespace OclPrototype2
 {
@@ -27,18 +29,48 @@ namespace OclPrototype2
         public uint m_index;
         private State m_state;
         public OCLPort m_port;
-        public uint m_size;
+        
+        // Structure contains information for the buffer
+        [StructLayout(LayoutKind.Sequential)]
+        public struct OCLBufferInfoStruct
+        {
+            public uint length;
+            public uint operation_or_exception_ordinal;
+            public uint maxLength;
+        }
+        public OCLBufferInfoStruct[] m_currentBufferInfo;
+        public ComputeBuffer<OCLBufferInfoStruct> m_computeBufferCurrentBufferInfo;
+        public GCHandle m_currentBufferInfoHandle;
 
+#if MAP_UNMAP_APPROACH
+        public IntPtr m_mappedPtr;
+#endif
         public OCLBuffer(uint index_, OCLPort port_)
         {
             m_index = index_;
             m_port = port_;
             m_state = State.AVAILABLE;
+
+            m_currentBufferInfo = new OCLBufferInfoStruct[1];
+            m_currentBufferInfoHandle = GCHandle.Alloc(m_currentBufferInfo, GCHandleType.Pinned);
+            m_computeBufferCurrentBufferInfo = new ComputeBuffer<OCLBufferInfoStruct>(m_port.m_container.m_context, ComputeMemoryFlags.ReadWrite, 1);
+
+            m_currentBufferInfo[0].maxLength = OCLPort.BUFFER_LEN;
         }
 
         public void allocateByUser()
         {
             m_state = State.ALLOCATED_BY_USER;
+
+// The problem with the map/unmap approach is that the subsequent unmap operation
+// on the component input port data will copy the entire buffer's worth of data
+// to the device when the input_port_buffer->put() function is called.  But a size
+// may be passed to the put() operation that is smaller than the buffer size
+#if MAP_UNMAP_APPROACH
+            if (m_port.m_type == OCLPort.Type.INPUT)
+                m_mappedPtr = m_port.m_container.m_commandQueue.Map<float>(m_port.m_computeBufferArray[m_index], true, 
+                    ComputeMemoryMappingFlags.Write, 0, OCLPort.BUFFER_LEN, null);
+#endif
         }
 
         public void allocateBySystem()
@@ -48,19 +80,55 @@ namespace OclPrototype2
 
         public float[] getBufferStorage()
         {
+#if MAP_UNMAP_APPROACH
+            // This is for C# only -- we must copy from an unmanaged ptr 
+            // to a managed array
+            if (m_port.m_type == OCLPort.Type.OUTPUT)
+                Marshal.Copy(m_mappedPtr, m_port.m_bufferArray[m_index], 0, OCLPort.BUFFER_LEN);
+#endif                
             return (m_port.m_bufferArray[m_index]);
         }
 
         public void put()
         {
             // Call this version of "put" for output buffers (in order to retire them)
+
+            if (m_port.m_type == OCLPort.Type.INPUT)
+                throw new OCLException("Should not call this version of put for an input buffer");
+
+#if MAP_UNMAP_APPROACH
+            if (m_port.m_type == OCLPort.Type.OUTPUT)
+            {
+                // I'm done with the output buffer so we can unmap it
+                m_port.m_container.m_commandQueue.Unmap(m_port.m_computeBufferArray[m_index], ref m_mappedPtr, null);
+            }
+#endif
             putBase();
         }
 
-        public void put(uint size_)
+        public uint getLength()
         {
-            // Call this version of "put" for input buffers (so we can specify the size)
-            m_size = size_;
+            return m_currentBufferInfo[0].length;
+        }
+
+        public uint getOpCode()
+        {
+            return m_currentBufferInfo[0].operation_or_exception_ordinal;
+        }
+
+        public void put(uint opcode_, uint size_)
+        {
+            // Note: the put operation should also include endOfData but I don't know what to do with it
+
+            // Call this version of "put" for input buffers (so we can specify the size and opcode)
+
+            if (m_port.m_type == OCLPort.Type.OUTPUT)
+                throw new OCLException("Should not call this version of put for an output buffer");
+            
+            
+            m_currentBufferInfo[0].length = size_;
+            m_currentBufferInfo[0].operation_or_exception_ordinal = opcode_;
+
             putBase();
         }
         private void putBase()
@@ -81,7 +149,7 @@ namespace OclPrototype2
 
                 m_port.setBufferForNextKernelExecution(this);
 
-                m_port.m_worker.run(this);
+                m_port.m_worker.writeBufferToDeviceAndRunWorkerIfWarranted(this);
             }
             else
             {
