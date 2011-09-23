9cbe4c0702ce1bce1ebe56c61e652749365ea106
John miller <jmiller@linuxserver.(none)>
2011-09-23 06:59:35 -0400

    Added OpenSplice Licence info

--- core/dataplane/msg_drivers/dds/src/OpenSpliceBindings.cxx
+++ core/dataplane/msg_drivers/dds/src/OpenSpliceBindings.cxx
@@ -1,4 +1,16 @@
 
+/*
+ *                         OpenSplice DDS
+ *
+ *   This software and documentation are Copyright 2006 to 2009 PrismTech
+ *   Limited and its licensees. All rights reserved. See file:
+ *
+ *                     $OSPL_HOME/LICENSE
+ *
+ *   for full copyright notice and license terms.
+ *
+ */
+
 #ifdef OPENSPLICE_MSG_SUPPORT
 
 #include <OpenSpliceBindings.h>
