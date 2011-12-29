ab7140f52bbb639d29718c0f8317ba8d72b9406e
Jim Kulp <jek@parera.com>
2011-12-29 11:33:16 -0500

    add three more xml app examples

---
+++ tools/cdk/examples/bias_xml/biasFile.cxx
@@ -0,0 +1,49 @@
+#include <cstdio>
+#include <cassert>
+#include <string>
+#include "OcpiApi.h"
+
+namespace OA = OCPI::API;
+
+int main(int argc, char **argv) {
+  std::string hello("<application>"
+		    // instance name defaults to file_read since there is only one
+		    "  <instance worker='file_read'>"
+		    "    <property name='filename' value='test.input'/>"
+		    "    <property name='granularity' value='4'/>"
+		    "    <property name='messageSize' value='16'/>"
+		    "  </instance>"
+		    "  <instance worker='bias'>"
+		    "    <property name='biasValue' value='0x01020304'/>"
+		    "  </instance>"
+		    "  <instance worker='file_write'>"
+		    "    <property name='filename' value='test.output'/>"
+		    "  </instance>"
+		    "  <connection>"
+		    "    <port instance='file_read' name='out'/>"
+		    "    <port instance='bias' name='in'/>"
+		    "  </connection>"
+		    "  <connection>"
+		    "    <port instance='bias' name='out'/>"
+		    "    <port instance='file_write' name='in'/>"
+		    "  </connection>"
+		    "</application>");
+  try {
+    OA::Application app(hello);
+    fprintf(stderr, "Application XML parsed and deployments (containers and implementations) chosen\n");
+    app.initialize();
+    fprintf(stderr, "Application established: containers, workers, connections all created\n");
+    fprintf(stderr, "Communication with the application established\n");
+    app.start();
+    fprintf(stderr, "Application started/running\n");
+    app.wait();
+    fprintf(stderr, "Application finished\n");
+    std::string name, value;
+    for (unsigned n = 0; app.getProperty(n, name, value); n++)
+      fprintf(stderr, "Property %2u: %s = \"%s\"\n", n, name.c_str(), value.c_str());
+    return 0;
+  } catch (std::string &e) {
+    fprintf(stderr, "Exception thrown: %s\n", e.c_str());
+  }
+  return 1;
+}
---
+++ tools/cdk/examples/cat_xml/catHello.cxx
@@ -0,0 +1,46 @@
+#include <cstdio>
+#include <cassert>
+#include <string>
+#include "OcpiApi.h"
+
+namespace OA = OCPI::API;
+
+int main(int argc, char **argv) {
+  std::string hello("<application>"
+		    // instance name defaults to file_read since there is only one
+		    "  <instance worker='file_read'>"
+		    "    <property name='filename' value='hello.file'/>"
+		    "    <property name='messageSize' value='4'/>"
+		    "  </instance>"
+		    "  <connection>"
+		    "    <external name='out'/>"
+		    "    <port instance='file_read' name='out'/>"
+		    "  </connection>"
+		    "</application>");
+  try {
+    OA::Application app(hello);
+    fprintf(stderr, "Application XML parsed and deployments (containers and implementations) chosen\n");
+    app.initialize();
+    fprintf(stderr, "Application established: containers, workers, connections all created\n");
+    OA::ExternalPort &ep = app.getPort("out");
+    fprintf(stderr, "Communication with the application established\n");
+    app.start();
+    fprintf(stderr, "Application started/running\n");
+    uint32_t length;
+    bool end;
+    do {
+      OA::ExternalBuffer *b;
+      uint8_t opcode;
+      uint8_t *data;
+      while (!(b = ep.getBuffer(data, length, opcode, end)))
+	sleep(1);
+      if (length > 0)
+	write(2, data, length);
+      b->release();
+    } while (length > 0 && !end);
+    return 0;
+  } catch (std::string &e) {
+    fprintf(stderr, "Exception thrown: %s\n", e.c_str());
+  }
+  return 1;
+}
