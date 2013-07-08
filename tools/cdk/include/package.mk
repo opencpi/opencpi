51815129680e308887b5093dee5f74426b9bf644
Jim Kulp <jek@parera.com>
2013-07-08 09:05:56 -0400

    add new file to repo

---
+++ tools/cdk/include/package.mk
@@ -0,0 +1,20 @@
+ifndef Package
+$(info The "Package" variable is not set. Assuming Package=local.)
+Package:=local
+endif
+PackageFile:=lib/package-name
+ifeq ($(wildcard lib),)
+$(shell mkdir -p lib)
+endif
+ifeq ($(wildcard $(PackageFile)),)
+  $(shell echo $(Package) > $(PackageFile))
+else
+  PackageName:=$(shell cat $(PackageFile))
+  ifneq ($(Package),$(PackageName))
+    ifneq ($(MAKECMDGOALS),clean)
+      $(error You must make clean after changing the package name.)
+    endif
+  endif
+endif
+clean::
+	$(AT)rm -f -r lib
