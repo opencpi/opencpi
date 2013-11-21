65dd680077c7354d3c3e82f6c2237e0af4544504
Jim Kulp <jek@parera.com>
2013-11-21 16:29:27 -0500

    missing file in repo

--- hdl/platforms/ml605/copy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Populate from drop (in addition to creating platform.mk by hand)
-DROP=$OCPI_HDL_IMPORTS_DIR
-# Source files that are specific to this platform
-cp $DROP/rtl/mkFTop_ml605.v .
-cp $DROP/libsrc/hdl/ocpi/fpgaTop_ml605.v .
-cp $DROP/ucf/ml605.ucf .
-cp $DROP/ucf/ml605.xcf .
-
