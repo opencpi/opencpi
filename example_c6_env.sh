cad90ee68ca2a0112cb7b06f59bc5d989b8ff2b6
Jim Kulp <jek@parera.com>
2015-09-23 10:17:23 -0400

    remove OpenCV from default c6 env; enable LD_LIBRARY_PATH during build

--- example_c6_env.sh
+++ example_c6_env.sh
@@ -19,7 +19,8 @@ export OCPI_BUILD_SHARED_LIBRARIES=0
 export OCPI_MODELSIM_DIR=/home/jek/mac/Mentor/modelsim_dlx
 export OCPI_MODELSIM_LICENSE_FILE=$OCPI_MODELSIM_DIR/../license-20150407.txt
 
-export OCPI_OPENCV_HOME=/opt/opencpi/prerequisites/opencv/linux-c6-x86_64
+# Enable this if you want to, and have, installed OPENCV
+# export OCPI_OPENCV_HOME=/opt/opencpi/prerequisites/opencv/linux-c6-x86_64
 
 . ./env/finish.sh
 done; trap - ERR
