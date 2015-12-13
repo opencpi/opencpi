5a111226174e0c078b9108a21bd7c70838c276f3
Jim Kulp <jek@parera.com>
2015-12-13 08:32:27 -0500

    missing new files

---
+++ platforms/centos7/centos7-target.sh
@@ -0,0 +1,9 @@
+# settings when targeting this platform.
+export OcpiLibraryPathEnv=LD_LIBRARY_PATH
+export OCPI_OCL_LIBS=-lOpenCL
+export OCPI_EXTRA_LIBS="rt dl pthread"
+export OCPI_EXPORT_DYNAMIC="-Xlinker --export-dynamic"
+export OcpiAsNeeded="-Xlinker --no-as-needed"
+export CC=gcc
+export CXX=c++
+export LD=c++
