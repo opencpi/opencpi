a9218221139165d9beec71dfc88120ee6a5cefb1
Jim Kulp <jek@parera.com>
2016-01-17 14:15:48 -0500

    make examples work when there is no explicit env setup.

--- examples/setup.mk
+++ examples/setup.mk
@@ -4,6 +4,8 @@ endif
 
 ifeq ($(filter clean%,$(MAKECMDGOALS)),)
   include $(OCPI_CDK_DIR)/include/ocpisetup.mk
+  export PATH:=$(OCPI_CDK_DIR)/bin/$(OCPI_TOOL_DIR):$(OCPI_CDK_DIR)/scripts:$(PATH)
+  export OCPI_LIBRARY_PATH=.:$(OCPI_CDK_DIR)/lib/components/rcc
 endif
 
 DIR=target-$(OCPI_TARGET_DIR)
