5cd86de997456ef618e10ca73a5d8c4701bfd41a
Jim Kulp <jek@parera.com>
2016-03-02 16:51:05 -0500

    new prerequisite installation script for the AMD OpenCL SDK

---
+++ scripts/install-amd-sdk.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+if [ "$UID" -eq 0 ]; then
+  echo This script should not be run as root, since that would not be a sand-boxed installation.
+  exit 1
+fi
+if test "$OCPI_TARGET_HOST" != "linux-c6-x86_64"; then
+ echo This script only works for Centos6 linux x86_64.
+ exit 1
+fi
+OCPI_AMD_SDK_VERSION=3.0
+OCPI_AMD_SDK_BUILD=130.135-GA
+TARBALL=AMD-APP-SDKInstaller-v${OCPI_AMD_SDK_VERSION}.${OCPI_AMD_SDK_BUILD}-linux64.tar.bz2
+INSTALLER=AMD-APP-SDK-v${OCPI_AMD_SDK_VERSION}.${OCPI_AMD_SDK_BUILD}-linux64.sh
+AMD_SDK_INSTALL_DIR=$OCPI_PREREQUISITES_INSTALL_DIR/amd-sdk/$OCPI_TARGET_HOST
+. ./scripts/setup-install.sh
+mkdir -p amd-sdk
+cd amd-sdk
+SUBDIR=amd-sdk-${OCPI_AMD_SDK_VERSION}
+if test -f $TARBALL; then
+  echo The distribution file for the ADM SDK, $TARBALL, exists and is being used.
+  echo Remove `pwd`/$TARBALL afterwards if you want save space, and maybe download it again.
+else
+  echo You need to downloading the distribution file: $TARBALL
+  echo It requires a click-through license.
+  echo It is at http://developer.amd.com/tools-and-sdks/opencl-zone/amd-accelerated-parallel-processing-app-sdk
+  echo Specify the ${OCPI_AMD_SDK_VERSION} version of the 64 bit linux SDK.
+  echo Download the file and copy it to `pwd`/$TARBALL
+  exit 1
+fi
+if test -d $SUBDIR; then
+  echo The source directory $SUBDIR exists, using it for this target: $OCPI_TARGET_HOST.
+  cd $SUBDIR
+else
+  echo Unpacking download file $TARBALL into directory $SUBDIR.
+  mkdir $SUBDIR
+  cd $SUBDIR
+  tar xjf ../$TARBALL
+  if [ ! -x $INSTALLER ] ; then
+    echo The tarball file $TARBALL did not contain the installer: $INSTALLER
+    exit 1
+  fi
+  echo Running the self-executing tar file to extract all the installation files.
+  ./$INSTALLER --noexec --target tmp
+  rm $INSTALLER
+  echo The installer files have been extracted into a tmp directory.
+  (cd tmp; HOME=$OCPI_PREREQUISITES_INSTALL_DIR/amd-sdk/installed ./install.sh --silent --acceptEULA y)
+  cd ..
+  rm -r -f $SUBDIR
+  rm -r -f $OCPI_TARGET_HOST
+  ln -s installed/AMDAPPSDK-$OCPI_AMD_SDK_VERSION $OCPI_TARGET_HOST
+fi
+echo ============= amd-sdk for $OCPI_TARGET_HOST built and installed in $OCPI_PREREQUISITES_INSTALL_DIR/amd-sdk/$OCPI_TARGET_HOST
+exit 0
