674775df8a6f058ab51b50e7ee865380bc4e6f19
Jim Kulp <jek@localhost.localdomain>
2016-06-01 16:16:26 -0400

    restore zed kernel headers that somehow were removed

---
+++ platforms/zed/release-2013.4/kernel-headers-xilinx-v2013.4/arch/arm/common/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_arch/arm/common/built-in.o :=  /home/jek/mac/Xilinx/14.6/ISE_DS/EDK/gnu/arm/lin/bin/arm-xilinx-linux-gnueabi-ld -EL    -r -o arch/arm/common/built-in.o arch/arm/common/firmware.o arch/arm/common/icst.o arch/arm/common/timer-sp.o 
---
+++ platforms/zed/release-2013.4/kernel-headers-xilinx-v2013.4/arch/arm/mach-vexpress/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_arch/arm/mach-vexpress/built-in.o :=  /home/jek/mac/Xilinx/14.6/ISE_DS/EDK/gnu/arm/lin/bin/arm-xilinx-linux-gnueabi-ld -EL    -r -o arch/arm/mach-vexpress/built-in.o arch/arm/mach-vexpress/v2m.o arch/arm/mach-vexpress/platsmp.o arch/arm/mach-vexpress/hotplug.o 
