dd13e97b886c92065c627769b8168723831b4922
Jim Kulp <jek@parera.com>
2016-06-30 12:50:44 -0400

    expose all GP ports, beyond the current M_GP0

--- hdl/primitives/zynq/zynq_pkg.vhd
+++ hdl/primitives/zynq/zynq_pkg.vhd
@@ -781,8 +781,10 @@ component processing_system7 is
 component zynq_ps is
   port(
     ps_out       : out   ps2pl_t;
-    m_axi_gp_in  : in    m_axi_gp_in_t;
-    m_axi_gp_out : out   m_axi_gp_out_t;
+    m_axi_gp_in  : in    m_axi_gp_in_array_t(0 to C_M_AXI_GP_COUNT-1);
+    m_axi_gp_out : out   m_axi_gp_out_array_t(0 to C_M_AXI_GP_COUNT-1);
+    s_axi_gp_in  : in    s_axi_gp_in_array_t(0 to C_S_AXI_GP_COUNT-1);
+    s_axi_gp_out : out   s_axi_gp_out_array_t(0 to C_S_AXI_GP_COUNT-1);
     s_axi_hp_in  : in    s_axi_hp_in_array_t(0 to C_S_AXI_HP_COUNT-1);
     s_axi_hp_out : out   s_axi_hp_out_array_t(0 to C_S_AXI_HP_COUNT-1)
     );
