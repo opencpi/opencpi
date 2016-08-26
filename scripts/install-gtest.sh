b585ee11974778b2a3cee8a0907dbde40a098a6e
Jim Kulp <jek@parera.com>
2016-08-26 12:01:03 -0400

    gtest was moved to github so the install script has to go there now

--- scripts/install-gtest.sh
+++ scripts/install-gtest.sh
@@ -4,9 +4,10 @@ OCPI_GTEST_VERSION=1.7.0
 source scripts/setup-install.sh
 mkdir -p gtest
 cd gtest
-rm -r -f gtest*
-curl -O http://googletest.googlecode.com/files/gtest-$OCPI_GTEST_VERSION.zip
-unzip gtest-$OCPI_GTEST_VERSION.zip
+rm -r -f release-* googletest*
+#curl -O http://googletest.googlecode.com/files/gtest-$OCPI_GTEST_VERSION.zip
+curl -O -L https://github.com/google/googletest/archive/release-$OCPI_GTEST_VERSION.zip
+unzip release-$OCPI_GTEST_VERSION.zip
 # From here on is per target
 rm -r -f build-$OCPI_TARGET_HOST
 mkdir build-$OCPI_TARGET_HOST
@@ -23,7 +24,7 @@ CXX=$OCPI_CROSS_BUILD_BIN_DIR/$OCPI_CROSS_HOST-c++
 LD=$OCPI_CROSS_BUILD_BIN_DIR/$OCPI_CROSS_HOST-c++
 AR=$OCPI_CROSS_BUILD_BIN_DIR/$OCPI_CROSS_HOST-ar
 fi
-dir=../gtest-$OCPI_GTEST_VERSION
+dir=../googletest-release-$OCPI_GTEST_VERSION
 $CXX -fPIC -I$dir/include -I$dir -c $dir/src/gtest-all.cc
 $AR -rs libgtest.a gtest-all.o
 mkdir -p $OCPI_PREREQUISITES_INSTALL_DIR/gtest/$OCPI_TARGET_HOST
