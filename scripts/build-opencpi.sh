d3655cd69457cf33fb4ae37ebe581d4f500182f2
Jim Kulp <jek@parera.com>
2017-09-08 21:57:15 -0400

    More tweaks for running and building examples in an OCL environment (see previous commit)
    examples/ptest/Makefile:  enable running without full env setup
    runtime/ocl-support/src/ocpiocl_main.cxx: recently introduced bug for listing OCL targets
    scripts/build-opencpi.sh: build OCL components if we have OpenCL present
    tools/cdk/include/ocl/ocl-make.mk: another missing env var fed to $(shell), and allow "all"
    tools/cdk/include/lib.mk: cause "make ocl" to be benign and not do anything on non-OpenCL systems

--- scripts/build-opencpi.sh
+++ scripts/build-opencpi.sh
@@ -43,7 +43,7 @@ echo ===========================================================================
 echo Now we will '"make"' the built-in RCC '(software)' components for $OCPI_TARGET_PLATFORM
 make rcc
 echo ================================================================================
-echo Now we will '"make"' the built-in OCL '(GPU)' components for available OCL platforms
+echo Now we will '"make"' the built-in OCL '(GPU)' components for the available OCL platforms
 make ocl
 echo ================================================================================
 echo Now we will '"make"' the examples for $OCPI_TARGET_PLATFORM
