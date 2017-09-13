3051c795a7909296ccad8a6d9f4a07ca5157ec7c
Aaron D. Marasco <Aaron.Marasco@BIA-Boeing.com>
2017-09-13 01:22:06 -0400

    which does not ship in docker. might be used elsewhere

--- platforms/centos7/centos7-packages.sh
+++ platforms/centos7/centos7-packages.sh
@@ -20,7 +20,7 @@
 # Install prerequisite packages for Centos6
 echo Installing standard extra packages using "yum"
 sudo yum -y groupinstall "development tools"
-echo Installing packages required: tcl pax python-devel fakeroot
-sudo yum -y install tcl pax python-devel fakeroot
+echo Installing packages required: tcl pax python-devel fakeroot which
+sudo yum -y install tcl pax python-devel fakeroot which
 echo Installing 32 bit libraries '(really only required for modelsim)'
 sudo yum -y install glibc.i686 libXft.i686 libXext.i686 ncurses-libs.i686
