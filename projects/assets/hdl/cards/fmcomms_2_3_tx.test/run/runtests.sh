4bc89d1bd2ca70039be4fd20f47feaa9f0ae4fc7
Jim Kulp <jek@parera.com>
2018-08-19 20:29:40 -0400

    missing av test files

---
+++ projects/assets/hdl/cards/fmcomms_2_3_tx.test/run/runtests.sh
@@ -0,0 +1,41 @@
+# This file is protected by Copyright. Please refer to the COPYRIGHT file
+# distributed with this source distribution.
+#
+# This file is part of OpenCPI <http://www.opencpi.org>
+#
+# OpenCPI is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# OpenCPI is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+DIR=target-$OCPI_TOOL_HOST
+PROG=$DIR/fmcomms_2_3_tx_test_app
+
+for var in "$@"; do
+  if [ "$var" == "verify" ]; then
+
+    OCPI_LIBRARY_PATH=$OCPI_LIBRARY_PATH:../../devices/ad9361_config_proxy.rcc/:../fmcomms_2_3_tx.rcc/:../fmcomms_2_3_rx.rcc/:./assemblies/empty/container-empty_ml605_cfg_1rx_1tx_fmcomms_2_3_lpc_lvds_cnt_1rx_1tx_bypassasm_fmcomms_2_3_lpc_LVDS_ml605/:./assemblies/empty/container-empty_zed_cfg_1rx_1tx_fmcomms_2_3_lpc_lvds_cnt_1rx_1tx_bypassasm_fmcomms_2_3_lpc_LVDS_zed/ ./$PROG #$OUT
+
+    X=$?
+    if [ "$X" != "0" ]; then
+      exit $X
+    fi
+  elif [ "$var" == "verifyswonly" ]; then
+    ./$PROG swonly
+    X=$?
+    if [ "$X" != "0" ]; then
+      exit $X
+    fi
+  fi
+done
+
+exit 0
