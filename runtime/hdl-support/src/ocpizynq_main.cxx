ba3ecadfa87660ddee5acac443aca701ccfc2bbb
Davis Hoover <dhoover@lab01.geontech.local>
2019-01-24 11:52:33 -0500

    updated ocpizynq utility to correctly error out on 'ocpizynq clocks' command for UltraScale+ architectures (since 'clocks' arg support does not exist yet)

--- runtime/hdl-support/src/ocpizynq_main.cxx
+++ runtime/hdl-support/src/ocpizynq_main.cxx
@@ -73,6 +73,23 @@ static uint8_t *map(off_t addr, size_t arg_size) {
   return ((uint8_t*)ptr) + (addr - base);
 }
 
+bool get_is_ultrascale_plus(volatile USP_AXI_HP *axi_hp) {
+  bool ret = false;
+  if (axi_hp) {
+    // i.e. S_AXI_HP0_FPD AFIFM RDDEBUG register's bitfield's
+    // AFI_VERSION == 'Zynq 8 Series', ref:
+    // https://www.xilinx.com/html_docs/registers/ug1087/ug1087-zynq-ultrascale-registers.html
+    ret = (axi_hp->afifm->rddebug & 0x40000000) == 0x40000000;
+  }
+  if (ret) {
+    printf("Detected Zynq architecture: UltraScale+\n");
+  }
+  else {
+    printf("Detected Zynq family: 7000\n");
+  }
+  return ret;
+}
+
 int
 mymain(const char **argv) {
 #if !defined(OCPI_ARCH_arm) && !defined(OCPI_ARCH_arm_cs) && !defined(OCPI_ARCH_arm64)
@@ -82,92 +99,7 @@ mymain(const char **argv) {
   std::string cmd = argv[0]; // ensured valid by caller
   if (cmd == "test")
     printf("test\n");
-  else if (cmd == "clocks") {
-    volatile FTM *ftm = (volatile FTM *)map(FTM_ADDR, sizeof(FTM));
-    if (!ftm)
-      return 1;
-    volatile SLCR *slcr = (volatile SLCR *)map(SLCR_ADDR, sizeof(SLCR));
-    if (!slcr)
-      return 1;
-    printf("ftm: glbctrl 0x%x status 0x%x control 0x%x cycountpre 0x%x synccount 0x%x "
-	   "itcyccount 0x%x lock_status 0x%x lock_access 0x%x\n",
-	   ftm->glbctrl, ftm->status, ftm->control, ftm->cycountpre, ftm->synccount,
-	   ftm->itcyccount, ftm->lock_status, ftm->lock_access);
-    ftm->lock_access = 0xc5acce55;
-    ftm->glbctrl = 1;
-    ftm->control = 4;
-    ftm->lock_access = 0;
-#ifdef OCPI_OS_macos
-    uint32_t t0 = 0;
-#else
-    struct timespec ts;
-    clock_gettime(CLOCK_MONOTONIC, &ts);
-    ts.tv_sec++;
-    uint32_t t0 = ftm->itcyccount;
-    clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &ts, NULL);
-#endif
-    uint32_t t1 = ftm->itcyccount;
-    double freq = (t1 - t0)/2.;
-    freq *= slcr->clk_621_true & 1 ? 6 : 4;
-    uint32_t ctrl = slcr->arm_clk_ctrl;
-    PLL
-      arm("ARM", slcr->arm_pll_ctrl, slcr->arm_pll_cfg),
-      ddr("DDR", slcr->ddr_pll_ctrl, slcr->ddr_pll_cfg),
-      io("IO", slcr->io_pll_ctrl, slcr->io_pll_cfg),
-      &pll = ((ctrl & 0x30) >> 4) == 2 ? ddr :
-             ((ctrl & 0x30) >> 4) == 3 ? io : arm;
-    uint32_t divisor = (ctrl >> 8) & 0x3f;
-    double pssclk = (freq * divisor) / pll.fdiv;
-    printf("Apparent CPU frequency is:~ %4.2f\n", (freq + 5000.)/1000000.);
-    printf("Apparent PSCLK frequency is: %4.2f\n", (pssclk + 5000.)/1000000.);
-    printf("Apparent PLL frequencies are: ARM: %4.2f, DDR: %4.2f, IO: %4.2f\n",
-	   (arm.freq(pssclk) + 5000.)/1000000.,
-	   (ddr.freq(pssclk) + 5000.)/1000000.,
-	   (io.freq(pssclk) + 5000.)/1000000.);
-    bool x6 = (slcr->clk_621_true & 1) != 0;
-    double topfreq = pll.freq(pssclk) / divisor;
-    double
-      x1 = topfreq / (x6 ? 6 : 4),
-      x2 = x1 * 2,
-      x3x2 = x1 * (x6 ? 3 : 2);
-    printf("ARM CPU Clocks: source: %s, mode: %s, divisor: %u, freq: %4.2f MHz\n",
-	   pll.name, x6 ? "6:3:2:1" : "4:2:2:1", divisor,
-	   (topfreq + 5000.) / 1000000);
-    printf("  Enabled: peri: %s, 1x: %s %4.2f, 2x: %s %4.2f, 3x2x: %s %4.2f, 6x4x: %s %4.2f\n",
-	   (ctrl & (1 << 28)) ? "on" : "off",
-	   (ctrl & (1 << 27)) ? "on" : "off",
-	   (x1 + 5000.)/1000000.,
-	   (ctrl & (1 << 26)) ? "on" : "off",
-	   (x2 + 5000.)/1000000.,
-	   (ctrl & (1 << 25)) ? "on" : "off",
-	   (x3x2 + 5000.)/1000000.,
-	   (ctrl & (1 << 24)) ? "on" : "off",
-	   (topfreq + 5000.)/1000000.);
-    volatile FCLK *fp = slcr->fpga;
-    for (unsigned n = 0; n < NFCLKS; n++, fp++) {
-      volatile FCLK f = *(FCLK*)fp;
-      //    printf("%u %p %08x %08x %08x %08x\n", n, fp,
-      //	   f.clk_ctrl, fp->thr_ctrl, fp->thr_count, fp->thr_sta);
-      PLL &fpll =
-	((f.clk_ctrl & 0x30) >> 4) == 2 ? arm :
-	((f.clk_ctrl & 0x30) >> 4) == 3 ? ddr : io;
-      uint32_t
-	divisor0 = (f.clk_ctrl >> 8) & 0x3f,
-	divisor1 = (f.clk_ctrl >> 20) & 0x3f;
-
-      printf("FCLK %u: source: %s PLL, divisor0: %2u, divisor1: %2u",
-	     n, fpll.name, divisor0, divisor1);
-      printf(", throttling %sreset, throttling %sstarted",
-	     f.thr_ctrl & 2 ? "is " : "not ", f.thr_ctrl & 1 ? "is " : "not ");
-      if (f.thr_count & 0xffff)
-	printf(", last count: %u", f.thr_count & 0xffff);
-      printf(", mode: %s", f.thr_sta & 0x10000 ? "debug/static" : "stopped/normal");
-      if (f.thr_sta & 0xffff)
-	printf(", current count: %u", f.thr_sta & 0xffff);
-      printf(", frequency: %4.2f MHz\n",
-	     ((((options.psclk() * fpll.fdiv) / divisor0 ) / divisor1) + 5000) / 1000000.);
-    }
-  } else if (cmd == "axi_hp") {
+  else {
     struct AFI {
       uint32_t
         rdchan_ctrl,
@@ -186,53 +118,133 @@ mymain(const char **argv) {
     struct AXI_HP {
       AFI afi[NAXI_HPS];
     };
-
-    bool is_ultra_scale_plus = false;
-
     volatile USP_AXI_HP *axi_hp = (volatile USP_AXI_HP *)map(USP_AXI_HP_ADDR, sizeof(USP_AXI_HP));
-    if (axi_hp) {
+    bool is_ultra_scale_plus = get_is_ultrascale_plus(axi_hp);
+    if (is_ultra_scale_plus) { // implies axi_hp != 0
       volatile USP_AFIFM *afifm = axi_hp->afifm;
-
-      // i.e. S_AXI_HP0_FPD AFIFM RDDEBUG register's bitfield's
-      // AFI_VERSION == 'Zynq 8 Series', ref:
-      // https://www.xilinx.com/html_docs/registers/ug1087/ug1087-zynq-ultrascale-registers.html
-      is_ultra_scale_plus = (afifm->rddebug & 0x40000000) == 0x40000000;
-
-      if (is_ultra_scale_plus) {
-        printf("Detected Zynq architecture: UltraScale+\n");
+      if (cmd == "clocks") {
+        printf("clocks command is net yet supported on UltraScale+ architectures\n");
+        return 1;
+      }
+      else if (cmd == "axi_hp") {
         for (unsigned n = 0; n < USP_NAXI_HPS; n++, afifm++) {
           printf("AXI_HP %u: rdctrl: 0x%x rdissue: 0x%x rdqos: 0x%x rddebug: 0x%x\n",
-	         n, afifm->rdctrl, afifm->rdissue, afifm->rdqos, afifm->rddebug);
+                  n, afifm->rdctrl, afifm->rdissue, afifm->rdqos, afifm->rddebug);
           printf("        : wrctrl: 0x%x wrissue: 0x%x wrqos: 0x%x\n",
-	         afifm->wrctrl, afifm->wrissue, afifm->wrqos);
+                 afifm->wrctrl, afifm->wrissue, afifm->wrqos);
           printf("        :  i_sts: 0x%x i_en: 0x%x i_dis: 0x%x i_mask: 0x%x control: 0x%x safety_chk 0x%x\n",
-	         afifm->i_sts, afifm->i_en, afifm->i_dis, afifm->i_mask, afifm->control, afifm->safety_chk);
+                 afifm->i_sts, afifm->i_en, afifm->i_dis, afifm->i_mask, afifm->control, afifm->safety_chk);
         }
       }
     }
-
-    if (!is_ultra_scale_plus) {
-      printf("Detected Zynq family: 7000\n");
-      const uint32_t AXI_HP_ADDR = 0xf8008000;
-      volatile AXI_HP *axi_hp = (volatile AXI_HP *)map(AXI_HP_ADDR, sizeof(AXI_HP));
-      if (!axi_hp)
-        return 1;
-      volatile AFI *afi = axi_hp->afi;
-      printf("AXI_HP_ADDR 0x%x axi_hp 0x%p afi 0x%p\n", AXI_HP_ADDR, axi_hp, afi);
-      sleep(10);
-      for (unsigned n = 0; n < NAXI_HPS; n++, afi++) {
-#if 1
-        printf("AXI_HP %u: rdctrl: 0x%x rdissue: 0x%x rdqos: 0x%x rdfifo: <unread> rddebug: 0x%x\n",
-	       n, afi->rdchan_ctrl, afi->rdchan_issuingcap, afi->rdqos,
-	       /*afi->rddatafifo_level,*/ afi->rddebug);
-        printf("        : wrctrl: 0x%x wrissue: 0x%x wrqos: 0x%x wrfifo: <unread> wrdebug: 0x%x\n",
-	       afi->wrchan_ctrl, afi->wrchan_issuingcap, afi->wrqos, /*afi->wrdatafifo_level,*/
-	       afi->wrdebug);
+    else {
+      if (cmd == "clocks") {
+        volatile FTM *ftm = (volatile FTM *)map(FTM_ADDR, sizeof(FTM));
+        if (!ftm)
+          return 1;
+        volatile SLCR *slcr = (volatile SLCR *)map(SLCR_ADDR, sizeof(SLCR));
+        if (!slcr)
+          return 1;
+        printf("ftm: glbctrl 0x%x status 0x%x control 0x%x cycountpre 0x%x synccount 0x%x "
+	       "itcyccount 0x%x lock_status 0x%x lock_access 0x%x\n",
+	       ftm->glbctrl, ftm->status, ftm->control, ftm->cycountpre, ftm->synccount,
+	       ftm->itcyccount, ftm->lock_status, ftm->lock_access);
+        ftm->lock_access = 0xc5acce55;
+        ftm->glbctrl = 1;
+        ftm->control = 4;
+        ftm->lock_access = 0;
+#ifdef OCPI_OS_macos
+        uint32_t t0 = 0;
 #else
-        printf("AXI_HP %u: rdctrl: 0x%x rdissue: 0x%x rdqos: 0x%x rdfifo: 0x%x rddebug: 0x%x\n",
-	       n, afi->rdchan_ctrl, afi->rdchan_issuingcap, afi->rdqos, afi->rddatafifo_level, 0);
+        struct timespec ts;
+        clock_gettime(CLOCK_MONOTONIC, &ts);
+      ts.tv_sec++;
+        uint32_t t0 = ftm->itcyccount;
+        clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &ts, NULL);
 #endif
+        uint32_t t1 = ftm->itcyccount;
+        double freq = (t1 - t0)/2.;
+        freq *= slcr->clk_621_true & 1 ? 6 : 4;
+        uint32_t ctrl = slcr->arm_clk_ctrl;
+        PLL
+          arm("ARM", slcr->arm_pll_ctrl, slcr->arm_pll_cfg),
+          ddr("DDR", slcr->ddr_pll_ctrl, slcr->ddr_pll_cfg),
+          io("IO", slcr->io_pll_ctrl, slcr->io_pll_cfg),
+          &pll = ((ctrl & 0x30) >> 4) == 2 ? ddr :
+                 ((ctrl & 0x30) >> 4) == 3 ? io : arm;
+        uint32_t divisor = (ctrl >> 8) & 0x3f;
+        double pssclk = (freq * divisor) / pll.fdiv;
+        printf("Apparent CPU frequency is:~ %4.2f\n", (freq + 5000.)/1000000.);
+        printf("Apparent PSCLK frequency is: %4.2f\n", (pssclk + 5000.)/1000000.);
+        printf("Apparent PLL frequencies are: ARM: %4.2f, DDR: %4.2f, IO: %4.2f\n",
+	       (arm.freq(pssclk) + 5000.)/1000000.,
+	       (ddr.freq(pssclk) + 5000.)/1000000.,
+	       (io.freq(pssclk) + 5000.)/1000000.);
+        bool x6 = (slcr->clk_621_true & 1) != 0;
+        double topfreq = pll.freq(pssclk) / divisor;
+        double
+          x1 = topfreq / (x6 ? 6 : 4),
+          x2 = x1 * 2,
+          x3x2 = x1 * (x6 ? 3 : 2);
+        printf("ARM CPU Clocks: source: %s, mode: %s, divisor: %u, freq: %4.2f MHz\n",
+	       pll.name, x6 ? "6:3:2:1" : "4:2:2:1", divisor,
+	       (topfreq + 5000.) / 1000000);
+        printf("  Enabled: peri: %s, 1x: %s %4.2f, 2x: %s %4.2f, 3x2x: %s %4.2f, 6x4x: %s %4.2f\n",
+	       (ctrl & (1 << 28)) ? "on" : "off",
+	       (ctrl & (1 << 27)) ? "on" : "off",
+	       (x1 + 5000.)/1000000.,
+	       (ctrl & (1 << 26)) ? "on" : "off",
+	       (x2 + 5000.)/1000000.,
+	       (ctrl & (1 << 25)) ? "on" : "off",
+	       (x3x2 + 5000.)/1000000.,
+	       (ctrl & (1 << 24)) ? "on" : "off",
+	       (topfreq + 5000.)/1000000.);
+        volatile FCLK *fp = slcr->fpga;
+        for (unsigned n = 0; n < NFCLKS; n++, fp++) {
+          volatile FCLK f = *(FCLK*)fp;
+          //    printf("%u %p %08x %08x %08x %08x\n", n, fp,
+          //	   f.clk_ctrl, fp->thr_ctrl, fp->thr_count, fp->thr_sta);
+          PLL &fpll =
+	((f.clk_ctrl & 0x30) >> 4) == 2 ? arm :
+	((f.clk_ctrl & 0x30) >> 4) == 3 ? ddr : io;
+          uint32_t
+	    divisor0 = (f.clk_ctrl >> 8) & 0x3f,
+	    divisor1 = (f.clk_ctrl >> 20) & 0x3f;
+
+          printf("FCLK %u: source: %s PLL, divisor0: %2u, divisor1: %2u",
+	         n, fpll.name, divisor0, divisor1);
+          printf(", throttling %sreset, throttling %sstarted",
+	         f.thr_ctrl & 2 ? "is " : "not ", f.thr_ctrl & 1 ? "is " : "not ");
+          if (f.thr_count & 0xffff)
+	    printf(", last count: %u", f.thr_count & 0xffff);
+          printf(", mode: %s", f.thr_sta & 0x10000 ? "debug/static" : "stopped/normal");
+          if (f.thr_sta & 0xffff)
+	    printf(", current count: %u", f.thr_sta & 0xffff);
+          printf(", frequency: %4.2f MHz\n",
+	         ((((options.psclk() * fpll.fdiv) / divisor0 ) / divisor1) + 5000) / 1000000.);
+        }
+      } else if (cmd == "axi_hp") {
+        const uint32_t AXI_HP_ADDR = 0xf8008000;
+        volatile AXI_HP *axi_hp = (volatile AXI_HP *)map(AXI_HP_ADDR, sizeof(AXI_HP));
+        if (!axi_hp)
+          return 1;
+        volatile AFI *afi = axi_hp->afi;
+        printf("AXI_HP_ADDR 0x%x axi_hp 0x%p afi 0x%p\n", AXI_HP_ADDR, axi_hp, afi);
         sleep(10);
+        for (unsigned n = 0; n < NAXI_HPS; n++, afi++) {
+#if 1
+          printf("AXI_HP %u: rdctrl: 0x%x rdissue: 0x%x rdqos: 0x%x rdfifo: <unread> rddebug: 0x%x\n",
+	         n, afi->rdchan_ctrl, afi->rdchan_issuingcap, afi->rdqos,
+	         /*afi->rddatafifo_level,*/ afi->rddebug);
+          printf("        : wrctrl: 0x%x wrissue: 0x%x wrqos: 0x%x wrfifo: <unread> wrdebug: 0x%x\n",
+	         afi->wrchan_ctrl, afi->wrchan_issuingcap, afi->wrqos, /*afi->wrdatafifo_level,*/
+	         afi->wrdebug);
+#else
+          printf("AXI_HP %u: rdctrl: 0x%x rdissue: 0x%x rdqos: 0x%x rdfifo: 0x%x rddebug: 0x%x\n",
+	         n, afi->rdchan_ctrl, afi->rdchan_issuingcap, afi->rdqos, afi->rddatafifo_level, 0);
+#endif
+          sleep(10);
+        }
       }
     }
   }
